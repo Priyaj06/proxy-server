a9f6808af65958af679708e26ccfeb15ecb2bfec
Priya Jayagopalan <pjayago@Priya.local>
2015-08-25 23:43:17 -0700

    updated index.js

--- index.js
+++ index.js
@@ -11,26 +11,21 @@ let port = argv.port || (argv.host === '127.0.0.1' ? 8000 : 80)
 
 let destinationUrl = argv.url || scheme + argv.host + ':' + port
 let logPath = argv.log && path.join(__dirname, argv.log)
-let logStream = logPath ? fs.createWriteStream(logPath) : process.stdoutt
+let logStream = logPath ? fs.createWriteStream(logPath) : process.stdout
 
 
 http.createServer((req, res) => {
-	    console.log(`Request received at: ${req.url}`)
-	    //res.end('hello world\n')
 	    for (let header in req.headers) {
 		        res.setHeader(header, req.headers[header])
 	    }
 	process.stdout.write('\n\n\n' + JSON.stringify(req.headers))
-	//req.pipe(process.stdout)
-	//req.pipe(getLogStream())
+
 	req.pipe(logStream, {end: false})
 	logStream.write('Request headers: ' + JSON.stringify(req.headers))
 	req.pipe(res)
 }).listen(8000)
 
 http.createServer((req, res) => {
-	  console.log(`Proxying request to: ${destinationUrl + req.url}`)
-	  // Proxy code here
 	  if(req.headers['x-destination-url']) {
 	  	destinationUrl = req.headers['x-destination-url']
 	  }
@@ -43,5 +38,4 @@ http.createServer((req, res) => {
 	process.stdout.write(JSON.stringify(downstreamResponse.headers))
 	downstreamResponse.pipe(process.stdout)
 	downstreamResponse.pipe(res)
-//	req.pipe(request(options)).pipe(res)
 }).listen(8001)
