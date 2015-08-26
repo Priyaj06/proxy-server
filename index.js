70f6d61c60e467f9e3bc426b8d1109dd8787176c
Priya Jayagopalan <pjayago@Priya.local>
2015-08-25 23:32:57 -0700

    updated files

--- index.js
+++ index.js
@@ -1,52 +1,47 @@
 let http = require('http')
 let request = require('request')
-// Place near the top of your file, just below your other requires 
-// Set a the default value for --host to 127.0.0.1
-let argv = require('yargs')
-    .default('host', '127.0.0.1')
-    .argv
-
-let scheme = 'http://'
-// Get the --port value
-// If none, default to the echo server port, or 80 if --host exists
-let port = argv.port || (argv.host === '127.0.0.1' ? 8000 : 80)
-// Update our destinationUrl line from above to include the port
-let destinationUrl = argv.url || scheme + argv.host + ':' + port
 let path = require('path')
 let fs = require('fs')
+let argv = require('yargs')
+    .default('host', '127.0.0.1')
+        .argv
+	let scheme = 'http://'
 
+let port = argv.port || (argv.host === '127.0.0.1' ? 8000 : 80)
+
+let destinationUrl = argv.url || scheme + argv.host + ':' + port
 let logPath = argv.log && path.join(__dirname, argv.log)
-let logStream = logPath ? fs.createWriteStream(logPath) : process.stdout
+let logStream = logPath ? fs.createWriteStream(logPath) : process.stdoutt
+
 
 http.createServer((req, res) => {
-    console.log(`Request received at: ${req.url}`)
-
-    req.pipe(res)
-
-	for (let header in req.headers) {
-	    res.setHeader(header, req.headers[header])
-	}
-
+	    console.log(`Request received at: ${req.url}`)
+	    //res.end('hello world\n')
+	    for (let header in req.headers) {
+		        res.setHeader(header, req.headers[header])
+	    }
 	process.stdout.write('\n\n\n' + JSON.stringify(req.headers))
+	//req.pipe(process.stdout)
+	//req.pipe(getLogStream())
 	req.pipe(logStream, {end: false})
 	logStream.write('Request headers: ' + JSON.stringify(req.headers))
 	req.pipe(res)
 }).listen(8000)
 
 http.createServer((req, res) => {
-	 console.log(`Proxying request to: ${destinationUrl + req.url}`)
-	 let options = {
-	        headers: req.headers,
-	        url: `http://${destinationUrl}${req.url}`
-	    }
- 
- 	//request(options).pipe(res)
- 	options.method = req.method
-	// Notice streams are chainable:
-	// inpuStream -> input/outputStream -> outputStream
-	//req.pipe(request(options)).pipe(res)
+	  console.log(`Proxying request to: ${destinationUrl + req.url}`)
+	  // Proxy code here
+	  if(req.headers['x-destination-url']) {
+	  	destinationUrl = req.headers['x-destination-url']
+	  }
+	  let options = {
+	  	headers: req.headers,
+		url: `${destinationUrl}${req.url}`
+	  }
+	options.method = req.method
 	let downstreamResponse = req.pipe(request(options))
 	process.stdout.write(JSON.stringify(downstreamResponse.headers))
 	downstreamResponse.pipe(process.stdout)
 	downstreamResponse.pipe(res)
+//	req.pipe(request(options)).pipe(res)
 }).listen(8001)
